7d0405e96fe2c63f1664a888f0a9d77065c443be
jesuisundev <admin@jesuisundev.com>
2020-07-03 00:25:15 -0400

    article struct lin

---
+++ struclin/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int foo [3] = { 1998, 2006, 2018 };  
+    
+    cout << foo[1] << endl;
+
+    return 0;
+}
---
+++ struclin/array.py
@@ -0,0 +1,5 @@
+from array import *
+
+foo = array('i', [1998, 2006, 2018])
+
+print(foo[2])

42ff41b4a539277db89861a6c140093b24ebe152
jesuisundev <admin@jesuisundev.com>
2020-09-17 09:03:19 -0400

    hello world

--- threejs/public/libs/app.js
+++ threejs/public/libs/app.js
@@ -1,19 +1,31 @@
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({antialias: true})
 
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 
 scene.add(cube)
 camera.position.z = 5
+
+renderer.setClearColor("#e5e5e5")
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight
+
+  camera.updateProjectMatrix()
+})
+
 function animate () {
   requestAnimationFrame(animate)
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera)
 }
+
 animate()

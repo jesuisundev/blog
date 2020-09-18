eb4a3b181ceaf5cd254676754f9dce76e17842e0
jesuisundev <admin@jesuisundev.com>
2020-09-18 17:33:36 -0400

    WIp

--- threejs/public/libs/app.js
+++ threejs/public/libs/app.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer({antialias: true})
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
@@ -9,23 +9,22 @@ const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 camera.position.z = 5
 
-renderer.setClearColor("#e5e5e5")
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
 
-  camera.updateProjectMatrix()
+    camera.updateProjectMatrix()
 })
 
-function animate () {
-  requestAnimationFrame(animate)
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera)
+function animate() {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera)
 }
 
 animate()

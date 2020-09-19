dee57ca85abff8f9adf5146ecfd8025bfc7c86dd
jesuisundev <admin@jesuisundev.com>
2020-09-18 22:59:00 -0400

    first cylinder

--- threejs/public/libs/app.js
+++ threejs/public/libs/app.js
@@ -2,13 +2,22 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-const cube = new THREE.Mesh(geometry, material)
+const geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
+const material = new THREE.MeshBasicMaterial({ wireframe: true })
+const cylinder = new THREE.Mesh(geometry, material)
+var light = new THREE.PointLight(0xFFFF00);
+light.position.set(0, 0, 0);
+scene.add(light);
 
-scene.add(cube)
-camera.position.z = 5
 
+scene.add(cylinder)
+
+camera.position.z = 0
+camera.position.x = 0
+camera.position.y = 15
+camera.lookAt(0, 0, 0)
+
+cylinder.flipSided = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
@@ -22,8 +31,7 @@ window.addEventListener('resize', () => {
 
 function animate() {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cylinder.rotation.y += 0.01;
     renderer.render(scene, camera)
 }
 

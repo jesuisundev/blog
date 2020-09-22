1ff299b62d1cc58c9e8dbee60cf65b37c92c9ae2
jesuisundev <admin@jesuisundev.com>
2020-09-22 08:49:29 -0400

    second cylinder

--- threejs/public/libs/app.js
+++ threejs/public/libs/app.js
@@ -1,16 +1,18 @@
 // TODO
 
-// PARTICLE
+// POST PROCESS EFFECT
+// BLUR/BLOOM SECOND CYLINDER
+// DEPTH OF FIELD FIRST CYLINDER
 // SHAKING CAMERA
 // MOVE CAMERA TO BOTTOM
+// GOD-RAYS FROM 0 TO 1 opacitu with max density
 // BACKGROUND TO WHITE SLOWLY THEN FAST
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 
-scene.fog = new THREE.FogExp2(0xe4dcff, 0.05)
-
+let clock = new THREE.Clock();
 let cylinderRadiusTop = 1
 let cylinderRadiusBottom = 1
 let cylinderHeight = 20
@@ -30,11 +32,29 @@ const geometry = new THREE.CylinderGeometry(
     cylinderHeightSegments,
     cylinderOpenEnded
 )
-const material = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: texture })
+const material = new THREE.MeshPhongMaterial({ transparent: true, side: THREE.DoubleSide, map: texture, blending: THREE.AdditiveBlending })
 const cylinder = new THREE.Mesh(geometry, material)
 
 
+// second cylinder
+const secondTexture = new THREE.TextureLoader().load("/images/colorfull.jpg");
+secondTexture.wrapS = THREE.RepeatWrapping;
+secondTexture.wrapT = THREE.RepeatWrapping;
+secondTexture.repeat.set(1, 1);
+const secondGeometry = new THREE.CylinderGeometry(
+    cylinderRadiusTop,
+    cylinderRadiusBottom,
+    cylinderHeight,
+    cylinderRadiusSegments,
+    cylinderHeightSegments,
+    cylinderOpenEnded
+)
+const secondMaterial = new THREE.MeshPhongMaterial({ transparent: true, side: THREE.DoubleSide, map: secondTexture, blending: THREE.AdditiveBlending, opacity: 0.4 })
+const secondCylinder = new THREE.Mesh(secondGeometry, secondMaterial)
+
+
 scene.add(cylinder)
+scene.add(secondCylinder)
 
 camera.position.x = 0
 camera.position.y = 10
@@ -62,6 +82,10 @@ function animate() {
     cylinder.rotation.y += 0.001
     texture.offset.y -= 0.0008;
     texture.offset.x -= 0.0008;
+
+    secondCylinder.rotation.y += 0.001
+    secondTexture.offset.y -= 0.0018;
+    secondTexture.offset.x -= 0.0008;
     //texture.offset.y %= 1;
     //texture.needsUpdate = true;
     // TODO => cant change geometry after its created

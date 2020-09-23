5963a5d9710682668fa5255be63f17872de42c9e
jesuisundev <admin@jesuisundev.com>
2020-09-23 00:40:52 -0400

    beautiful

--- threejs/public/libs/app.js
+++ threejs/public/libs/app.js
@@ -1,18 +1,21 @@
 // TODO
 
 // POST PROCESS EFFECT
-// BLUR/BLOOM SECOND CYLINDER
+
 // DEPTH OF FIELD FIRST CYLINDER
 // SHAKING CAMERA
 // MOVE CAMERA TO BOTTOM
 // GOD-RAYS FROM 0 TO 1 opacitu with max density
 // BACKGROUND TO WHITE SLOWLY THEN FAST
-
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer({ antialias: true })
+const renderer = new THREE.WebGLRenderer({
+    powerPreference: "high-performance",
+    antialias: false,
+    stencil: false,
+    depth: false
+})
 
-let clock = new THREE.Clock();
 let cylinderRadiusTop = 1
 let cylinderRadiusBottom = 1
 let cylinderHeight = 20
@@ -20,10 +23,10 @@ let cylinderRadiusSegments = 32
 let cylinderHeightSegments = 3
 let cylinderOpenEnded = true
 
-const texture = new THREE.TextureLoader().load("/images/dark.jpg");
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(1, 1);
+const texture = new THREE.TextureLoader().load('/images/dark.jpg')
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
+texture.repeat.set(1, 1)
 const geometry = new THREE.CylinderGeometry(
     cylinderRadiusTop,
     cylinderRadiusBottom,
@@ -32,15 +35,19 @@ const geometry = new THREE.CylinderGeometry(
     cylinderHeightSegments,
     cylinderOpenEnded
 )
-const material = new THREE.MeshPhongMaterial({ transparent: true, side: THREE.DoubleSide, map: texture, blending: THREE.AdditiveBlending })
+const material = new THREE.MeshPhongMaterial({
+    transparent: true,
+    side: THREE.DoubleSide,
+    map: texture,
+    blending: THREE.AdditiveBlending
+})
 const cylinder = new THREE.Mesh(geometry, material)
 
-
 // second cylinder
-const secondTexture = new THREE.TextureLoader().load("/images/colorfull.jpg");
-secondTexture.wrapS = THREE.RepeatWrapping;
-secondTexture.wrapT = THREE.RepeatWrapping;
-secondTexture.repeat.set(1, 1);
+const secondTexture = new THREE.TextureLoader().load('/images/colorfull.jpg')
+secondTexture.wrapS = THREE.RepeatWrapping
+secondTexture.wrapT = THREE.MirroredRepeatWrapping
+secondTexture.repeat.set(1, 1)
 const secondGeometry = new THREE.CylinderGeometry(
     cylinderRadiusTop,
     cylinderRadiusBottom,
@@ -49,12 +56,41 @@ const secondGeometry = new THREE.CylinderGeometry(
     cylinderHeightSegments,
     cylinderOpenEnded
 )
-const secondMaterial = new THREE.MeshPhongMaterial({ transparent: true, side: THREE.DoubleSide, map: secondTexture, blending: THREE.AdditiveBlending, opacity: 0.4 })
+const secondMaterial = new THREE.MeshPhongMaterial({
+    transparent: true,
+    side: THREE.DoubleSide,
+    map: secondTexture,
+    blending: THREE.AdditiveBlending,
+    opacity: 0.4
+})
 const secondCylinder = new THREE.Mesh(secondGeometry, secondMaterial)
 
+// third cylinder
+//const thirdTexture = new THREE.TextureLoader().load('/images/perseidas.jpg')
+const thirdTexture = new THREE.TextureLoader().load('/images/meteoritniy-dogd-nebo-meteoriti.jpg')
+thirdTexture.wrapS = THREE.RepeatWrapping
+thirdTexture.wrapT = THREE.MirroredRepeatWrapping
+thirdTexture.repeat.set(1, 1)
+const thirdGeometry = new THREE.CylinderGeometry(
+    cylinderRadiusTop,
+    cylinderRadiusBottom,
+    cylinderHeight,
+    cylinderRadiusSegments,
+    cylinderHeightSegments,
+    cylinderOpenEnded
+)
+const thirdMaterial = new THREE.MeshPhongMaterial({
+    transparent: true,
+    side: THREE.DoubleSide,
+    map: thirdTexture,
+    blending: THREE.AdditiveBlending,
+    opacity: 0.1
+})
+const thirdCylinder = new THREE.Mesh(thirdGeometry, thirdMaterial)
 
 scene.add(cylinder)
 scene.add(secondCylinder)
+scene.add(thirdCylinder)
 
 camera.position.x = 0
 camera.position.y = 10
@@ -69,28 +105,54 @@ const intensity = 1
 const light = new THREE.AmbientLight(color, intensity)
 scene.add(light)
 
-
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight)
     camera.aspect = window.innerWidth / window.innerHeight
-
-    //camera.updateProjectMatrix()
+    camera.updateProjectMatrix()
 })
+
+const bloomEffect = new POSTPROCESSING.BloomEffect({
+    blendFunction: POSTPROCESSING.BlendFunction.ADD,
+    kernelSize: POSTPROCESSING.KernelSize.SMALL
+});
+
+// tweak this for the bloom effect
+bloomEffect.blendMode.opacity.value = 4;
+
+// postprocessing effect pass instance
+const effectPass = new POSTPROCESSING.EffectPass(
+    camera,
+    bloomEffect
+);
+
+// enable effect pass
+effectPass.renderToScreen = true;
+const composer = new POSTPROCESSING.EffectComposer(renderer);
+// postprocessing mandatory first render pass
+composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+// postprocessing effect render pass
+composer.addPass(effectPass);
 
 function animate() {
     requestAnimationFrame(animate)
     cylinder.rotation.y += 0.001
-    texture.offset.y -= 0.0008;
-    texture.offset.x -= 0.0008;
+    texture.offset.y -= 0.0008
+    texture.offset.x -= 0.0008
 
     secondCylinder.rotation.y += 0.001
-    secondTexture.offset.y -= 0.0018;
-    secondTexture.offset.x -= 0.0008;
-    //texture.offset.y %= 1;
-    //texture.needsUpdate = true;
-    // TODO => cant change geometry after its created
-    //cylinder.geometry.parameters.radiusBottom += 1
-    renderer.render(scene, camera)
+    secondTexture.offset.y -= 0.0018
+    secondTexture.offset.x -= 0.0008
+
+    thirdCylinder.rotation.y += 0.001
+    thirdTexture.offset.y -= 0.0058
+    thirdTexture.offset.x -= 0.0008
+        // texture.offset.y %= 1
+        // texture.needsUpdate = true
+        // TODO => cant change geometry after its created
+        // cylinder.geometry.parameters.radiusBottom += 1
+    composer.render()
+        //renderer.render(scene, camera)
+        //renderer.render(scene, camera)
 }
 window.scene = scene
 animate()

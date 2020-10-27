60a70bbce5e5490ffabdd3aedd336e2e3a67ee3e
jesuisundev <admin@jesuisundev.com>
2020-10-27 19:52:07 -0400

    Setup webcam stream

--- yousmileyouloose/index.js
+++ yousmileyouloose/index.js
@@ -1,6 +1,16 @@
 const express = require("express")
+const https = require('https');
 const path = require("path")
+const fs = require('fs');
 const app = express()
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(8080, () => console.log('Server on port 8080'))
+
+https.createServer({
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+        passphrase: ''
+    }, app)
+    .listen(8080);
+
+//app.listen(8080, () => console.log('Server on port 8080'))
--- yousmileyouloose/public/libs/app.js
+++ yousmileyouloose/public/libs/app.js
@@ -1,1 +1,66 @@
-console.log('hello world!')
+document.getElementById('actualshit').addEventListener('click', event => setupFaceDetection(event))
+
+const webcam = document.getElementById("webcam");
+webcam.addEventListener("play", refreshState);
+
+/**
+ * Launch the whole process following thoses steps
+ * - Load models from faceapi
+ * - Ask user for the video stream and setup the video
+ *
+ * @async
+ * @param {Object} event event of the click
+ */
+async function setupFaceDetection(event) {
+    event.preventDefault()
+
+    document.getElementById('home').remove()
+
+    await loadModels()
+    setupWebcam()
+}
+
+/**
+ * Load models from faceapi
+ * @async
+ */
+async function loadModels() {
+    await faceapi.nets.tinyFaceDetector.loadFromUri(
+        "https://192.168.5.39:8080/models"
+    );
+    await faceapi.nets.faceExpressionNet.loadFromUri(
+        "https://192.168.5.39:8080/models"
+    );
+}
+
+/**
+ * Setup the webcam stream for the user.
+ * On success, the stream of the webcam is set to the source of the HTML5 tag.
+ * On error, the error is logged and the process continue.
+ */
+function setupWebcam() {
+    navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then(stream => {
+            webcam.srcObject = stream;
+        })
+        .catch(err => console.error("can't found your camera :(", err));
+}
+
+/**
+ * Set an refresh interval where the faceapi will scan the face of the subject
+ * and return an object of the most likely expressions.
+ * Use this detection data to pick an expression and spread background gifs on divs.
+ * @async
+ */
+async function refreshState() {
+    setInterval(async() => {
+        // const detections = await faceapi
+        //     .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        //     .withFaceExpressions();
+
+        // if (detections && detections[0] && detections[0].expressions) {
+        //     spreadByExpression(getCurrentExpression(detections[0].expressions));
+        // }
+    }, 500);
+}
